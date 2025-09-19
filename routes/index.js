334c0a83e510b4a6beeb90b528c80807c7077f4f
VarunVerma09 <umangsoni409@gmail.com>
2025-09-19 23:17:19 +0530

    Added Post model

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-var express = require('express');
+let express = require('express');
 var router = express.Router();
 const useModel = require("./users"); // make sure path is correct
 const passport = require('passport');
 const LocalStrategy = require("passport-local");
+const upload = require('./multer')
+const postModel = require('./post')
 
 passport.use(new LocalStrategy(useModel.authenticate()));
 
@@ -16,6 +18,34 @@ router.get('/', function (req, res, next) {
 router.get('/login', function (req, res, next) {
   res.render('login',{error: req.flash("error")});
   });
+router.get('/feed',isLoggedIn, function (req, res, next) {
+  res.render('feed');
+  });
+  
+  router.post('/upload', upload.single("file"), async function (req, res, next) {
+if(!res.file){
+ return res.status(404).send("No Files Were Given");
+}
+const user = await useModel.findOne({username: req.session.passport.user});
+const postData= await postModel.create({
+  image: req.file.filename,
+  imageText: req.body.filecaption,
+  user: user._id,
+});
+user.post.push(postData._id);
+await user.save();
+res.send("done")
+
+
+});
+
+
+router.get('/profile', isLoggedIn, async function (req, res) {
+  const user = await useModel.findOne({
+    username:req.session.passport.user
+  })
+  res.render('profile',{user});
+});
 
 
 router.post('/register', function (req, res) {
@@ -39,22 +69,13 @@ router.post('/login', passport.authenticate("local", {
   failureFlash:true,
 }), function (req, res) {});
 
-
-
-
-router.get('/profile', isLoggedIn, async function (req, res) {
-  const user = await useModel.findOne({
-    username:req.session.passport.user
-  })
-  res.render('profile',{user});
-});
-
 router.get('/logout', function (req, res, next) {
   req.logout(function (err) {
     if (err) { return next(err); }
     res.redirect('/');
   });
 });
+
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
